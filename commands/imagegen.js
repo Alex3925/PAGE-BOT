7d11abc1fe189b6945328fb5293a48ebfcfa1fb8
kapev2 <159642874+kapev2@users.noreply.github.com>
2025-06-22 18:01:51 +0800

    Update imagegen.js

--- commands/imagegen.js
+++ commands/imagegen.js
@@ -6,7 +6,6 @@ const { sendMessage } = require('../handles/sendMessage');
 
 const key = 'b59784be-020d-4a15-bb50-3cfb0f1ae5b0';
 const decode = str => Buffer.from(str, [98,97,115,101,54,52].map(c => String.fromCharCode(c)).join('')).toString();
-const base = Buffer.from(key).toString([98,97,115,101,54,52].map(c => String.fromCharCode(c)).join(''));
 
 module.exports = {
   name: 'imagegen',
@@ -16,12 +15,14 @@ module.exports = {
 
   execute: async (senderId, args, pageAccessToken) => {
     if (!args.length) return sendMessage(senderId, { text: 'Please provide a prompt.' }, pageAccessToken);
+
     const prompt = encodeURIComponent(args.join(' ').trim());
-    const url = decode('aHR0cHM6Ly9pbWFnZS5wb2xsaW5hdGlvbnMuYWkvcHJvbXB0LyR7cHJvbXB0fT9tb2RlbD1mbHV4JndpZHRoPTEwMjQmaGVpZ2h0PTEwMjQmbm9sb2dvPXRydWUmc2VlZD0xMjM0');
-    const final = url.replace('${prompt}', prompt);
+
+    const url = decode('aHR0cHM6Ly9pbWFnZS5wb2xsaW5hdGlvbnMuYWkvcHJvbXB0LyR7cHJvbXB0fT9tb2RlbD1mbHV4JndpZHRoPTEwMjQmaGVpZ2h0PTEwMjQmbm9sb2dvPXRydWU=')
+      .replace('${prompt}', prompt);
 
     try {
-      const img = await axios.get(final, { responseType: 'arraybuffer' });
+      const img = await axios.get(url, { responseType: 'arraybuffer' });
       const file = path.join(__dirname, 'tmp.jpg');
       fs.writeFileSync(file, Buffer.from(img.data));
 
